0165a7b9e83b7f783bdb7c825ae1bee395ee2d81
Hyosik Philip Joo <lux1937@gmail.com>
2025-01-06 10:14:11 -0800

    fix: allow http request

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,20 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Match all routes
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; connect-src http: https:; img-src 'self' data: https: http:; script-src 'self' 'unsafe-inline' https: http:;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
